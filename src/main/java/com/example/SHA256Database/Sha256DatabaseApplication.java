664197a0d9cd22ed2f0ef9e481be0bd030df1a32
Ben Jamison <u1173514@utah.edu~>
2023-03-05 18:43:27 -0700

    Automatically add sample hashes to database on startup.

--- src/main/java/com/example/SHA256Database/Sha256DatabaseApplication.java
+++ src/main/java/com/example/SHA256Database/Sha256DatabaseApplication.java
@@ -1,5 +1,8 @@
 package com.example.SHA256Database;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,13 +14,71 @@ import org.springframework.boot.CommandLineRunner;
 public class Sha256DatabaseApplication implements CommandLineRunner {
 
   @Autowired
-  HashRepository hashRepository;
+  private HashRepository hashRepository;
+
+  private String[] sampleInputs = new String[] {
+    "Hello world!",
+    "omega",
+    "Ben Jamison",
+    "ffff",
+    "password123",
+    "test",
+    "sample",
+    "SHA-256",
+    "SHA 256",
+    "Hexadecimal",
+    "UTF-8",
+    "[Object object]",
+    "Base 64",
+    "Java",
+    "Spring Framework",
+    "Javascript",
+    "Typescript",
+    "Angular",
+    "Bootstrap",
+    "HTML",
+    "CSS",
+    "JUnit",
+    "Levi",
+    "Neal",
+    "Brigham",
+    "Luke",
+    "Emily", 
+    "12345",
+    "9876543210",
+    "0123456789",
+    "SHA256Database",
+    "hashme",
+    "donthashme",
+    "Git",
+    "Linux",
+    "Vim",
+    "VS Code",
+    "Radagon",
+    "Godrick",
+    "Godfrey",
+    "Marika",
+    "Malenia",
+    "Miquella",
+    "Rykard",
+    "Morgott"
+  };
 
   public static void main(String[] args) {
     SpringApplication.run(Sha256DatabaseApplication.class, args);
   }
 
   public void run(String ... args) {
-    System.out.println("Command line runner is running...");
+    Random rand = new Random();
+
+    for (int i = 0; i < sampleInputs.length; i++) {
+      Hash h = new Hash(sampleInputs[i].getBytes(StandardCharsets.UTF_8));
+
+      h.setCount(rand.nextInt(10000));
+
+      hashRepository.save(h);
+    }
+
+    System.out.println("Inserted sample hashes.");
   }
 }
